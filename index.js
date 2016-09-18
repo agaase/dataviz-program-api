e9277e4d7a6015ecf68a3cc30df1f7edc542fe49
ASEEM AGGARWAL <aseemaggarwal@ASEEMs-MBP-2.home>
2016-09-17 23:49:53 -0400

    updating files

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var req = require('request');
+var cors = require('cors');
+
+var app = express();
+app.use(cors());
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get("/",function(request, response) {
+  response.render('home');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+app.post("/get",function(request,response){
+
+});
